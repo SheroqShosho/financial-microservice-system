19582274fdf0edfc1492136c637fd21fb04d7eba
oliver <oliverkalthoff@gmail.com>
2026-01-15 14:55:25 +0100

    Lagt till .env och application.properties samt fixat product-model

--- src/main/java/se/omegapoint/productdirectory/models/Product.java
+++ src/main/java/se/omegapoint/productdirectory/models/Product.java
@@ -1,4 +1,25 @@
 package se.omegapoint.productdirectory.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import se.omegapoint.productdirectory.models.enums.ProductStatus;
+import se.omegapoint.productdirectory.models.enums.ProductType;
+
+@Entity
 public abstract class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    private Integer productId;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private ProductType productType;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private ProductStatus productStatus;
 }
