b9a7c1162f31a423e0170f5718739c8cd5869e87
mitar98 <mitarmitrovic98@gmail.com>
2026-01-16 13:48:13 +0100

    Skapade creditcard modellerna

--- src/main/java/se/omegapoint/productdirectory/models/Product.java
+++ src/main/java/se/omegapoint/productdirectory/models/Product.java
@@ -1,11 +1,9 @@
 package se.omegapoint.productdirectory.models;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import se.omegapoint.productdirectory.models.enums.ProductStatus;
 import se.omegapoint.productdirectory.models.enums.ProductType;
-
-import static jakarta.persistence.InheritanceType.JOINED;
 
 
 @Entity
@@ -27,7 +25,8 @@ public abstract class Product {
     @NotNull
     private ProductStatus productStatus;
 
-    protected Product() {}
+    protected Product() {
+    }
 
     protected Product(ProductType productType, ProductStatus productStatus) {
         this.productType = productType;
@@ -41,6 +40,7 @@ public abstract class Product {
     public ProductType getProductType() {
         return productType;
     }
+
     public void setProductType(ProductType productType) {
         this.productType = productType;
     }
@@ -48,6 +48,7 @@ public abstract class Product {
     public ProductStatus getProductStatus() {
         return productStatus;
     }
+
     public void setProductStatus(ProductStatus productStatus) {
         this.productStatus = productStatus;
     }
