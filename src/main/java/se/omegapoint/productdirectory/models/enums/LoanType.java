24b100ba21b86083c3f952a4b3a32cea9e690648
Fredde <fredrik.liljeblad@gritacademy.se>
2026-01-16 13:04:42 +0100

    Skapade Loan-modellerna

---
+++ src/main/java/se/omegapoint/productdirectory/models/enums/LoanType.java
@@ -0,0 +1,6 @@
+package se.omegapoint.productdirectory.models.enums;
+
+public enum LoanType {
+    PRIVATE_LOAN,
+    MORTGAGE
+}
--- src/main/java/se/omegapoint/productdirectory/models/loans/MortgageLoan.java
+++ src/main/java/se/omegapoint/productdirectory/models/loans/MortgageLoan.java
@@ -1,4 +1,36 @@
 package se.omegapoint.productdirectory.models.loans;
 
-public class MortgageLoan {
+import jakarta.persistence.Entity;
+import se.omegapoint.productdirectory.models.enums.LoanType;
+import se.omegapoint.productdirectory.models.enums.ProductStatus;
+import se.omegapoint.productdirectory.models.enums.ProductType;
+
+import java.math.BigDecimal;
+
+@Entity
+public class MortgageLoan extends Loan {
+
+    protected MortgageLoan() {
+    }
+
+    protected MortgageLoan(
+                        LoanType loanType,
+                        BigDecimal minAmount,
+                        BigDecimal maxAmount,
+                        int durationMonths,
+                        BigDecimal interestRate,
+                        ProductType productType,
+                        ProductStatus productStatus
+    ) {
+
+        super(
+                loanType,
+                minAmount,
+                maxAmount,
+                durationMonths,
+                interestRate,
+                productType,
+                productStatus
+        );
+    }
 }
