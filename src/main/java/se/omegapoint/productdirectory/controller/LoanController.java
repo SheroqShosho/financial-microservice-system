87c375e15f73c349ea14108e08bfd43d984484dd
oliver <oliverkalthoff@gmail.com>
2026-01-22 15:55:11 +0100

    Lagt till loggning

--- src/main/java/se/omegapoint/productdirectory/controller/LoanController.java
+++ src/main/java/se/omegapoint/productdirectory/controller/LoanController.java
@@ -1,5 +1,7 @@
 package se.omegapoint.productdirectory.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,8 @@ import java.util.List;
 @RequestMapping("/api/loan")
 public class LoanController {
 
+    private static final Logger log = LoggerFactory.getLogger(LoanController.class);
+
     private final LoanService loanService;
 
     // Konstruktor för injection
@@ -24,6 +28,7 @@ public class LoanController {
     // Hanterar GET-request och hämtar alla lån. Returnerar dto-lista samt statuskod
     @GetMapping
     public ResponseEntity<List<LoanResponseDTO>> getAll() {
+        log.info("Get all loans");
         List<LoanResponseDTO> allLoans = loanService.getAllLoans(); // Skapar lista via service
 
         return ResponseEntity.status(HttpStatus.OK).body(allLoans); // Returnerar response med listan och statuskod
@@ -32,6 +37,7 @@ public class LoanController {
     // Hanterar POST-request och skapar nytt lån i databas. Returnerar sedan dto-response med body och statuskod
     @PostMapping
     public ResponseEntity<LoanResponseDTO> create(@RequestBody LoanRequestDTO request) {
+        log.info("Create loan request: {}", request);
         LoanResponseDTO response = loanService.addLoan(request); // Skickar request till service och får tillbaka response
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response); // Returnerar response till client med body och statuskod
