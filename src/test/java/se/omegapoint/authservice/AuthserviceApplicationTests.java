0a7bde4111049a224de8f47ff1ebef72cf4b166f
Sheroq <ssheroq@gmail.com>
2026-03-19 11:11:10 +0100

    Skapat projekt med databas koppling

---
+++ src/test/java/se/omegapoint/authservice/AuthserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.omegapoint.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
